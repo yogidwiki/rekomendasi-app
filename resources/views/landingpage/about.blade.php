a88f2e046c45bbae13c2eec4b6131c86b3ef09cb
rizkaamelia31 <rizkaamelia935@gmail.com>
2023-07-19 18:33:00 +0700

    push

--- resources/views/landingpage/about.blade.php
+++ resources/views/landingpage/about.blade.php
@@ -4,10 +4,12 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
+                <p>At web parenting, we envision a digital landscape where children can explore the internet confidently, harnessing it’ss vast potential while staying protected from potential risks. We believe in fostering open communication, promoting digital literacy, and encouraging responsible online behavior, creating a nurturing environment where children can thrive online..</p>
 
             </div>
             <div class="col-md-6">
-                
+                <img src="public/image/team.jpg" alt="Gambar Anda" style="max-width: 100%; height: auto;">
+
             </div>
         </div>
     </div>
