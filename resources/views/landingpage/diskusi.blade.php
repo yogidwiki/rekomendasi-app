9f891c0b7c62c149c0a1d63a46dbbfbc5f2b1f23
ninanoptrina19 <203200113@almaata.ac.id>
2023-07-19 19:46:01 +0700

    kategori diskusi slicing

--- resources/views/landingpage/diskusi.blade.php
+++ resources/views/landingpage/diskusi.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.landingpage')
 
 @section('content')
-    <h1 class="text-center mt-5">halaman diskusi</h1>
+    <h2 class="text-center mt-5">halaman diskusi</h2> 
+    <h1 class="text-center mt-5">    </h1> 
+    
+    <!-- First row with three buttons -->
+
+    
+    <style>
+    /* Custom CSS to center the buttons */
+    .centered-buttons {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 100px;
+    }
+    </style>
+
+<div class="container">
+    <div class="row d-flex justify-content-center mb-4">
+      <div class="col-md-2" >
+      <button type="button" class="btn btn-primary w-100">Starting A Family </button>
+      </div>
+      <div class="col-md-2">
+      <button type="button" class="btn btn-primary w-100"> A Family</button>
+      </div>
+      <div class="col-md-2">
+      <button type="button" class="btn btn-primary w-100">Starting A </button>
+      </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-2" >
+      <button type="button" class="btn btn-primary w-100">Starting A Family</button>
+      </div>
+      <div class="col-md-2">
+      <button type="button" class="btn btn-primary w-100"> A Family</button>
+      </div>
+      <div class="col-md-2">
+      <button type="button" class="btn btn-primary w-100">Starting A </button>
+      </div>
+    </div>
+  </div>
+
+
 @endsection
