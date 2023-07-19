5f3b9a1783948e60ae0fbc10ff516c625c98537b
Almirawk <almirawulank@gmail.com>
2023-07-19 21:34:18 +0700

    coba

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,13 @@
-@extends('layouts.app')
+@extends('layouts.landingpage')
+
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-md-4">
+            <!-- Gambar di sebelah kiri -->
+            <img src="path-to-your-image.jpg" alt="Image" />
+        </div>
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
@@ -11,10 +16,11 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
+                        <!-- Formulir register di sebelah kanan -->
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
