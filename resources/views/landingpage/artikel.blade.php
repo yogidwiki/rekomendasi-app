4e2eabfd84f26399c94e759ef3c01901d4dd4a3e
hael <haikaljawahir434@gmail.com>
2023-07-19 17:57:45 +0700

    add folder image

--- resources/views/landingpage/artikel.blade.php
+++ resources/views/landingpage/artikel.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.landingpage')
 @section('content')
 <h1 class="text-center mt-5">halaman artikel</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+            
+        </div>
+    </div>
+</div>
 @endsection
