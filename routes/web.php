7d75156fc191bb744977f910bfb0f0586473efdf
hael <haikaljawahir434@gmail.com>
2023-07-23 23:08:33 +0700

    FIX Route member

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LandingpageController;
 use App\Http\Controllers\TestimonialController;
+use App\Http\Controllers\MemberController;
 
 /*
 |--------------------------------------------------------------------------
